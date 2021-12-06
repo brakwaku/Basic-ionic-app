cccb196d097fad43d35a1c46429fc8bca9eadabe
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2021-12-06 16:27:38 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const calculateBtn = document.getElementById('calc-btn');
+const resetBtn = document.getElementById('reset-btn');
+const heightInput = document.getElementById('height-input');
+const weightInput = document.getElementById('weight-input');
+
+const resultArea = document.getElementById('result');
+
+const resetInputs = () => {
+    heightInput.value = '';
+    weightInput.value = '';
+}
+
+const calculateBMI = () => {
+    const enteredHeight = +heightInput.value;
+    const enteredWeight = +weightInput.value;
+
+    const bmi = enteredWeight / (enteredHeight * enteredHeight);
+
+    if (isNaN(bmi)) {
+        alert('Not a number, please check your inputs!');
+        return;
+    }
+
+    const resultElement = document.createElement('ion-card');
+
+    resultElement.innerHTML = `
+        <ion-card-content>
+            <h2>${bmi}</h2>
+        </ion-card-content>
+    `;
+    resultArea.innerHTML = '';
+    resultArea.appendChild(resultElement);
+}
+
+calculateBtn.addEventListener('click', calculateBMI);
+resetBtn.addEventListener('click', resetInputs);
